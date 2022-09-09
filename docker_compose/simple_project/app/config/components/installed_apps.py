e685cb8349ff27eab06b4a53fca247eb6b3d8ffe
Stanislav Matveew <stanislav.matveew@yandex.ru>
2022-09-09 22:13:56 +0300

    настроил swagger

--- docker_compose/simple_project/app/config/components/installed_apps.py
+++ docker_compose/simple_project/app/config/components/installed_apps.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080",]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080",
+                        "http://localhost:8080"]
 
 
