f0b435bacf9c31673735504f35e7e028d3071202
Stanislav Matveew <stanislav.matveew@yandex.ru>
2022-09-10 23:32:05 +0300

    вынес команды из manage.py как инструкция по первичной настройке

--- docker_compose/simple_project/app/entrypoint.sh
+++ docker_compose/simple_project/app/entrypoint.sh
@@ -8,30 +8,7 @@ done
 
 echo "PostgreSQL started"
 
-echo "python manage.py migrate"
-python manage.py migrate & wait
-echo "python manage.py createcachetable --noinput || true"
-python manage.py createcachetable --noinput || true & wait
 
-if [ "$DJANGO_SUPERUSER_USERNAME" ]
-then
-    echo "python manage.py createsuperuser --noinput || true"
-    python manage.py createsuperuser --noinput || true & wait
-
-fi
-
-echo "python manage.py collectstatic --noinput || true"
-python manage.py collectstatic --noinput || true & wait
-
-cd sqlite_to_postgres
-
-echo "python load_data.py"
-python load_data.py -e $psql_DB_NAME=movies_db $psql_DB_USER=app $psql_DB_PASSWORD=Njhufi33 $psql_DB_HOST=psql $psql_DB_PORT=5432
-
-# sqlite
-sqlite_DB_NAME=db.sqlite& wait
-
-cd ../
 echo "uwsgi --strict --ini uwsgi.ini"
 uwsgi --strict --ini uwsgi.ini
 
