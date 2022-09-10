964fa90cfe9aa375e373f2326288e0aca8cd3433
Stanislav Matveew <stanislav.matveew@yandex.ru>
2022-09-10 12:31:47 +0300

    настроил api для тестирования в postman

--- docker_compose/simple_project/app/movies/api/v1/views.py
+++ docker_compose/simple_project/app/movies/api/v1/views.py
@@ -2,16 +2,16 @@ from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
 from django.http import JsonResponse
 from django.views.generic.list import BaseListView
-
+from django.views.generic.detail import BaseDetailView
 from movies.models import Filmwork
-from movies.models.person_film_work import PersonFilmwork,Roles
+from movies.models.person_film_work import PersonFilmwork, Roles
 
-class MoviesListApi(BaseListView):
+class MoviesApiMixin:
     model = Filmwork
     http_method_names = ['get']  # Список методов, которые реализует обработчик
-    paginate_by = 50
+
     def get_queryset(self):
-        queryset = Filmwork.objects.prefetch_related('genres', 'person').values().all().\
+        queryset = Filmwork.objects.prefetch_related('genres', 'person').values().all(). \
             annotate(genres=ArrayAgg('genres__name',
                                      distinct=True),
                      actors=ArrayAgg('persons__full_name',
@@ -24,13 +24,19 @@ class MoviesListApi(BaseListView):
                                       filter=Q(personfilmwork__role__icontains=Roles.WRITER),
                                       distinct=True)
                      )
-        
+
         return queryset
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
 
+class MoviesListApi(MoviesApiMixin,BaseListView):
+
+    paginate_by = 50
+
+    def get_context_data(self, *, object_list=None, **kwargs):
         queryset = self.get_queryset()
-        paginator, page, queryset, is_paginated = self.paginate_queryset(queryset,self.paginate_by)
+        paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, self.paginate_by)
         context = {
             'count': paginator.count,
             'total_pages': paginator.num_pages,
@@ -40,5 +46,9 @@ class MoviesListApi(BaseListView):
         }
         return context
 
-    def render_to_response(self, context, **response_kwargs):
-        return JsonResponse(context)
+class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
+
+    def get_context_data(self, **kwargs):
+        return kwargs['object']
+
+
